09ca1c014131c88d6d7c6f23909e8928ebb407ca
muammertopcu <muammertop@outlook.com>
2024-08-05 23:03:17 +0300

    implemented: home screen

---
+++ src/screen/Home/Home.tsx
@@ -0,0 +1,11 @@
+import {Container, Title} from "@/components";
+
+const Home = () => {
+  return (
+    <Container>
+      <Title title={'Hello There!'}/>
+    </Container>
+  )
+}
+
+export default Home
